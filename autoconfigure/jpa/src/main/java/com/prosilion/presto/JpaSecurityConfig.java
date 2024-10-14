598c4c22d3f5210a6d86b88443c76b5c9462799b
nick avlo <avlo@yahoo.com>
2024-10-13 18:52:58 -0700

    refactoring jpa componenets into webCommon and security configs

--- autoconfigure/jpa/src/main/java/com/prosilion/presto/JpaSecurityConfig.java
+++ autoconfigure/jpa/src/main/java/com/prosilion/presto/JpaSecurityConfig.java
@@ -3,20 +3,11 @@ package com.prosilion.presto;
 import com.prosilion.presto.jpa.controller.JpaAuthController;
 import com.prosilion.presto.security.service.AuthUserService;
 import com.prosilion.presto.web.controller.AuthController;
-import com.prosilion.presto.web.model.AppUserDtoIF;
-import com.prosilion.presto.web.service.AppUserDtoService;
-import com.prosilion.presto.web.service.AppUserDtoServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 
 @Slf4j
 @AutoConfiguration
@@ -27,40 +18,5 @@ public class JpaSecurityConfig {
   @ConditionalOnMissingBean(name = "authController")
   AuthController authController(AuthUserService authUserService) {
     return new JpaAuthController(authUserService);
-  }
-
-  @Bean
-  @ConditionalOnMissingBean
-  public AuthenticationSuccessHandler defaultAuthenticationSuccessHandler() {
-    return new DefaultLoginHandler();
-  }
-
-  @Bean
-  @DependsOn("mvc")
-  @ConditionalOnMissingFilterBean
-  public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc, AuthenticationSuccessHandler authenticationSuccessHandler) throws Exception {
-    log.info("Loading JPA - Endpoint authorization configuration");
-    http.authorizeHttpRequests(authorize -> authorize
-        .requestMatchers(mvc.pattern("/css/**")).permitAll()
-        .requestMatchers(mvc.pattern("/images/**")).permitAll()
-        .requestMatchers(mvc.pattern("/register")).permitAll()
-        .requestMatchers(mvc.pattern("/users/**")).hasRole("USER")
-        .anyRequest().authenticated() // anyRequest() defines a rule chain for any request which did not match the previous rules
-    ).formLogin(form -> form
-            .loginPage("/login")
-            .loginProcessingUrl("/loginuser")
-//				.defaultSuccessUrl("/users")
-            .successHandler(authenticationSuccessHandler)
-            .permitAll()
-    ).logout(logout -> logout
-        .logoutRequestMatcher(mvc.pattern("/logout")).permitAll()
-    );
-    return http.build();
-  }
-
-  @Bean
-  @ConditionalOnMissingBean
-  public AppUserDtoService<AppUserDtoIF> appUserDtoService(AuthUserService authUserService) {
-    return new AppUserDtoServiceImpl<>(authUserService);
   }
 }
--- autoconfigure/nostr/src/main/java/com/prosilion/presto/NostrSecurityConfig.java
+++ autoconfigure/nostr/src/main/java/com/prosilion/presto/NostrSecurityConfig.java
@@ -14,11 +14,11 @@ import com.prosilion.presto.web.controller.AuthController;
 import com.prosilion.presto.web.model.NostrAppUserDtoIF;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -28,7 +28,8 @@ import javax.sql.DataSource;
 
 @Slf4j
 @AutoConfiguration
-@AutoConfigureBefore({JpaSecurityConfig.class, SecurityCoreConfig.class})
+@EnableWebSecurity
+//@AutoConfigureBefore({SecurityCoreConfig.class})
 public class NostrSecurityConfig {
 
   @Bean(name = "authController")
@@ -36,36 +37,6 @@ public class NostrSecurityConfig {
   AuthController authController(NostrUserService nostrUserService) {
     return new NostrAuthController(nostrUserService);
   }
-
-  @Bean
-  @DependsOn("mvc")
-  public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc, AuthenticationSuccessHandler authenticationSuccessHandler) throws Exception {
-    log.info("Loading Nostr - Endpoint authorization configuration");
-    http.authorizeHttpRequests(authorize -> authorize
-            .requestMatchers(mvc.pattern("/css/**")).permitAll()
-            .requestMatchers(mvc.pattern("/images/**")).permitAll()
-            .requestMatchers(mvc.pattern("/register-nostr")).permitAll()
-            .requestMatchers(mvc.pattern("/register")).permitAll()
-            .requestMatchers(mvc.pattern("/users/**")).hasRole("USER")
-//            .requestMatchers(mvc.pattern("/users/**")).permitAll()
-            .anyRequest().authenticated() // anyRequest() defines a rule chain for any request which did not match the previous rules
-    ).formLogin(form -> form
-        .loginPage("/login")
-        .loginProcessingUrl("/loginuser")
-        .defaultSuccessUrl("/users")
-        .successHandler(authenticationSuccessHandler)
-        .permitAll()
-    ).logout(logout -> logout
-        .logoutRequestMatcher(mvc.pattern("/logout")).permitAll()
-    );
-    return http.build();
-  }
-
-//  @Bean
-//  @Primary
-//  NostrJdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
-//    return new NostrJdbcUserDetailsManager(dataSource);
-//  }
 
   @Bean(name = "userDetailsService")
   @Primary
