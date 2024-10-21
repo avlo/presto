3dcbe7ead3e398072ef7205d5b5af85594ad23f3
nick avlo <avlo@yahoo.com>
2024-10-21 14:02:33 -0700

    https /\ssl added

--- autoconfigure/nostr/src/main/java/com/prosilion/presto/NostrSecurityConfig.java
+++ autoconfigure/nostr/src/main/java/com/prosilion/presto/NostrSecurityConfig.java
@@ -11,23 +11,36 @@ import com.prosilion.presto.security.service.AppUserService;
 import com.prosilion.presto.security.service.CustomizableAppUserService;
 import com.prosilion.presto.web.model.NostrAppUserDtoIF;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 
 import javax.sql.DataSource;
 
 @Slf4j
 @AutoConfiguration
 @EnableWebSecurity
+@EnableWebSocketSecurity
 public class NostrSecurityConfig {
+
+  @Bean
+  AuthorizationManager<Message<?>> authorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
+    messages.simpDestMatchers("/contract/**").hasRole("USER")
+        .simpDestMatchers("/**").permitAll()
+        .anyMessage().authenticated();
+    return messages.build();
+  }
 
   @Bean
   @DependsOn("mvc")
