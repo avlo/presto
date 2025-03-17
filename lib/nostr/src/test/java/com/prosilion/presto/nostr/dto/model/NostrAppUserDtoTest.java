710ec050a5e21bdd9551b916e707a4ff89a7abfe
nick avlo <avlo@yahoo.com>
2025-03-17 01:44:46 -0700

    junit configured

---
+++ lib/nostr/src/test/java/com/prosilion/presto/nostr/dto/model/NostrAppUserDtoTest.java
@@ -0,0 +1,37 @@
+package com.prosilion.presto.nostr.dto.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class NostrAppUserDtoTest {
+
+	@Test
+	void getId() {
+		assertEquals(1, 1);
+	}
+
+	@Test
+	void getUsername() {
+		assertNotEquals(1, 2);
+	}
+
+	void getPassword() {
+	}
+
+	void getPubkey() {
+	}
+
+	void setId() {
+	}
+
+	void setUsername() {
+	}
+
+	void setPassword() {
+	}
+
+	void setPubkey() {
+	}
+}
